cdfc06de609c0387a43fcc7b2973622d85a02a5a
Ngô Thành Nhân <ngothanhnhan125@gmail.com>
2025-07-09 10:10:54 +0700

    adjust test_app.py

--- app/test_app.py
+++ app/test_app.py
@@ -1,17 +1,17 @@
 import pytest
-from app import main  # đảm bảo tên file chính của bạn là main.py
+from main import app  # import từ main.py trong cùng thư mục
 
 @pytest.fixture
 def client():
-    with main.test_client() as client:
+    with app.test_client() as client:
         yield client
 
-def test_home_route(client):
+def test_home(client):
     response = client.get('/')
     assert response.status_code == 200
     assert response.get_json() == {"message": "Welcome to the API!"}
 
-def test_about_route(client):
+def test_about(client):
     response = client.get('/about')
     assert response.status_code == 200
     assert response.get_json() == {"version": "1.0", "author": "Yagmur Ozden"}
